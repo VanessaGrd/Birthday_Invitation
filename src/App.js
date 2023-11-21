a4e3f55ac8ca82e6fc9178d696987b11cce2a5bd
VanessaGrd <suede210810@gmail.com>
2023-11-21 14:21:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Invit from "./components/Invit";
+
+function App() {
+  return (
+    <div className="bg-blue-700 h-[100vh]">
+      <Invit />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Invit.js
@@ -0,0 +1,6 @@
+export default function Invit () {
+
+    return (
+        <div className="text-black">Hello</div>
+    )
+}
