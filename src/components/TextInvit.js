3a215221eb2d2714586ad948cdb1961689c8fa54
VanessaGrd <suede210810@gmail.com>
2023-12-05 16:43:43 +0100

    add mail and sms send

--- src/components/TextInvit.js
+++ src/components/TextInvit.js
@@ -2,7 +2,20 @@ import warning from "../assets/warning.svg"
 import christine2 from "../assets/christine2.jpg";
 
 export default function TextInvit() {
+const handleSmsClick = () => {
+  const phoneNumber = "06.82.46.68.83"; // Remplacez par votre numéro de téléphone
+  const message = "Je confirme ma présence à la fête d'anniversaire le 16 mars 2024!";
+  const smsLink = `sms:${phoneNumber}?body=${encodeURIComponent(message)}`;
+  window.location.href = smsLink;
+};
 
+const handleEmailClick = () => {
+  const emailAddress = "vanessa.giraud.lyon@gmail.com"; // Remplacez par votre adresse e-mail
+  const subject = "Réponse pour la fête d'anniversaire";
+  const body = "Je confirme ma présence à la fête d'anniversaire le 16 mars 2024!";
+  const emailLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  window.location.href = emailLink;
+};
     return (
       <div className="bg-hero-pattern bg-center bg-contain  lg:bg-no-repeat lg:bg-secondary-50 lg:items-center h-full flex justify-around text-center flex-col font-abc px-4 text-primary-50 items-center">
         <h1 className=" text-4xl font-bold  mt-10">Félicitations ! </h1>
@@ -11,7 +24,10 @@ export default function TextInvit() {
         <p className="text-2xl font-medium mb-10">Au printemps elle va fêter ses 60 ans ! Nous comptons sur vous pour l'aider à souffler ses bougies.</p>
         <p className="text-2xl ">
           La fête aura lieu le
-          <p className="mt-4 mb-6 font-bold text-3xl"> samedi 16 mars 2024 à 12h</p>
+          <p className="mt-4 mb-6 font-bold text-3xl">
+            {" "}
+            samedi 16 mars 2024 <br></br>à 12h
+          </p>
         </p>
         <div className="flex flex-col justify-center items-center bg-primary-50 p-2 mb-8 rounded-md">
           <img src={warning} alt="warning" className="w-12" />
@@ -28,12 +44,14 @@ export default function TextInvit() {
           </span>{" "}
         </p>
 
-        <p className="text-xl  mb-10  rounded-lg ">
-          {" "}
-          <span className="font-bold"> Réponse souhaitée avant le 20/01/2024 </span>
-          <br></br>
-          <span> 06.82.46.68.83 | vanessa.giraud.lyon@gmail.com</span>{" "}
-        </p>
+        <div className="flex flex-row text-md mb-10">
+          <button className="bg-primary-50 text-secondary-50 px-4 py-2 rounded mr-4" onClick={handleSmsClick}>
+            Répondre par SMS <br></br>(06.82.46.68.83)
+          </button>
+          <button className="bg-primary-50 text-secondary-50 px-4 py-2 rounded" onClick={handleEmailClick}>
+            Répondre par E-mail (vanessa.giraud.lyon@gmail.com)
+          </button>
+        </div>
       </div>
     );
 }
